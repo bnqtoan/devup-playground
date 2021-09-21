e28571b747d66431a15ea0d23adeeae652d2812a
Toan Bui (Flexer) <bnqtoan@gmail.com>
2021-09-22 06:19:05 +0700

    add section 3, 4, 5

--- landingpage-1/tailwind.config.js
+++ landingpage-1/tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
           "h2": ["40px", {lineHeight: 1.5}],
           "h3": ["32px", {lineHeight: 1.5}],
           "h4": ["22px", {lineHeight: 1.5}],
+          "h6": ["16px", {lineHeight: 1.5}],
           "p-l": ["18px", {lineHeight: "28px"}]
         },
         backgroundImage: {
@@ -32,7 +33,16 @@ module.exports = {
           "dark" : {
             "DEFAULT" : '#1F2E35',
             "800": '#22343D'
-          }
+          },
+          "yellow": {
+              1000: "#FF9900",
+          },
+          "red": {
+            1000: "#F03E3D",
+          }, 
+          "blue": {
+            1000: "#4D8DFF",
+          }, 
         }
       },
     },
