6a3a8d6adff56113dec3f719d519d604b23b2f54
Toan Bui (Flexer) <bnqtoan@gmail.com>
2021-09-25 00:07:05 +0700

    add section 7, landing page 1

--- landingpage-1/tailwind.config.js
+++ landingpage-1/tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
       container: {
         center: true
       },
+      borderWidth: {
+          "3": "3px"
+      },
       fontFamily: {
         'body': ["'Poppins', sans-serif"]
       },
@@ -22,9 +25,14 @@ module.exports = {
           "h6": ["16px", {lineHeight: 1.5}],
           "p-l": ["18px", {lineHeight: "28px"}]
         },
+        backgroundPosition:{
+          'center-top' : 'center top'
+        },
         backgroundImage: {
             'bg-1': "url(../images/bg/bg-1.svg)",
             'bg-1-dark': "url(../images/bg/bg-1-dark.svg)",
+            'bg-2': "url(../images/bg/bg-2.svg)",
+            'bg-2-dark': "url(../images/bg/bg-2-dark.svg)",
         },
         colors: {
           "light" : '#BCD0E5',
@@ -32,7 +40,8 @@ module.exports = {
           "primary" : '#02897A',
           "dark" : {
             "DEFAULT" : '#1F2E35',
-            "800": '#22343D'
+            "800": '#22343D',
+            "900": '#2F281E'
           },
           "yellow": {
               1000: "#FF9900",
